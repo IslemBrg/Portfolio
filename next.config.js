da38d505150e8130f22047c5a3c9d4e3602833f4
Islem Bargaoui <islem.bargaoui.si@gmail.com>
2024-01-30 16:53:15 +0100

    fixed env variables not working

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ dotenv.config();
 
 const nextConfig = {
   reactStrictMode: true, // enabled react-strict mode
-
+  env: {
+    REACT_APP_YOUR_SERVICE_ID: process.env.REACT_APP_YOUR_SERVICE_ID,
+    REACT_APP_YOUR_TEMPLATE_ID: process.env.REACT_APP_YOUR_TEMPLATE_ID,
+    REACT_APP_YOUR_PUBLIC_KEY: process.env.REACT_APP_YOUR_PUBLIC_KEY,
+    CONTACT_FORM_ID: process.env.CONTACT_FORM_ID,
+  },
   images: {
     domains: ["i.ibb.co"],
   },
