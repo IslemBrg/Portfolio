7cbf9d4a46101f2f1d37233d380ec0714f9c1d04
Islem Bargaoui <islem.bargaoui.si@gmail.com>
2024-06-03 19:27:25 +0100

    fixed project page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import React from "react";
 import ThemeContextProvider from "../contexts/theme-context";
 import "../styles/globals.css";
 import 'react-toastify/dist/ReactToastify.css';
+import './projects/projectPage.css';
 import Head from 'next/head';
 
 const App = ({ Component, pageProps }) => {
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,11 @@
 import { Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import React, { useContext, useState } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { SingleProject } from "../../components";
 import Link from "../../components/link";
 import { ThemeContext } from "../../contexts/theme-context";
-import { projectsData } from "../../data/projectsData";
+import { projectsData } from "../../data/projects-data";
+import { useRouter } from "next/router";
 
 function ProjectPage() {
   const [search, setSearch] = useState("");
@@ -16,67 +16,73 @@ function ProjectPage() {
     return content.toLowerCase().includes(search.toLowerCase());
   });
 
-  const useStyles = makeStyles((t) => ({
-    search: {
+  const searchStyles = {
+    color: theme.tertiary,
+    width: "40%",
+    height: "2.75rem",
+    outline: "none",
+    border: "none",
+    borderRadius: "20px",
+    padding: "0.95rem 1rem",
+    fontFamily: "'Noto Sans TC', sans-serif",
+    fontWeight: 500,
+    fontSize: "0.9rem",
+    backgroundColor: theme.secondary,
+    boxShadow:
+      theme.type === "dark"
+        ? "inset 3px 3px 6px #ffffff10, inset -3px -3px 6px #00000060"
+        : "inset 3px 3px 6px #ffffffbd, inset -3px -3px 6px #00000030",
+    "&::placeholder": {
       color: theme.tertiary,
-      width: "40%",
-      height: "2.75rem",
-      outline: "none",
-      border: "none",
-      borderRadius: "20px",
-      padding: "0.95rem 1rem",
-      fontFamily: "'Noto Sans TC', sans-serif",
-      fontWeight: 500,
-      fontSize: "0.9rem",
-      backgroundColor: theme.secondary,
-      boxShadow:
-        theme.type === "dark"
-          ? "inset 3px 3px 6px #ffffff10, inset -3px -3px 6px #00000060"
-          : "inset 3px 3px 6px #ffffffbd, inset -3px -3px 6px #00000030",
-      "&::placeholder": {
-        color: theme.tertiary,
-      },
-      [t.breakpoints.down("sm")]: {
-        width: "350px",
-      },
     },
-    home: {
-      color: theme.secondary,
-      position: "absolute",
-      top: 25,
-      left: 25,
-      padding: "7px",
-      borderRadius: "50%",
-      boxSizing: "content-box",
-      fontSize: "2rem",
-      cursor: "pointer",
-      boxShadow:
-        theme.type === "dark"
-          ? "3px 3px 6px #ffffff40, -3px -3px 6px #00000050"
-          : "3px 3px 6px #ffffff40, -3px -3px 6px #00000050",
-      transition: "all 0.3s ease-in-out",
-      "&:hover": {
-        color: theme.tertiary,
-        transform: "scale(1.1)",
-      },
-      [t.breakpoints.down("sm")]: {
-        fontSize: "1.8rem",
-      },
-    },
-  }));
+  };
 
-  const classes = useStyles();
+  const homeStyles = {
+    color: theme.secondary,
+    position: "absolute",
+    top: 25,
+    left: 25,
+    padding: "7px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    boxShadow:
+      theme.type === "dark"
+        ? "3px 3px 6px #ffffff40, -3px -3px 6px #00000060"
+        : "3px 3px 6px #ffffff40, -3px -3px 6px #00000030",
+    transition: "all 0.3s ease-in-out",
+    zIndex: 1000,
+    "&:hover": {
+      color: theme.tertiary,
+      transform: "scale(1.1)",
+    },
+  };
+
+  const router = useRouter()
 
   return (
     <div className="projectPage" style={{ backgroundColor: theme.secondary }}>
       <div
-        className="projectPage-header"
-        style={{ backgroundColor: theme.primary }}
+        style={{
+          backgroundColor: theme.primary,
+          width: "100%",
+          display: "flex",
+          borderRadius: "0 0 20px 20px",
+        }}
       >
-        <Link href="/">
-          <AiOutlineHome className={classes.home} />
-        </Link>
-        <h1 style={{ color: theme.secondary }}>Projects</h1>
+        <div
+          style={{
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "fit-content",
+          }}
+        >
+            <AiOutlineHome size={50} style={homeStyles} onClick={()=>router.push("/")}/>
+        </div>
+        <div className="projectPage-header">
+          <h1 style={{ color: theme.secondary }}>Projects</h1>
+        </div>
       </div>
       <div className="projectPage-container">
         <div className="projectPage-search">
@@ -85,7 +91,7 @@ function ProjectPage() {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             placeholder="Search project..."
-            className={classes.search}
+            style={searchStyles}
           />
         </div>
         <div className="project-container">
